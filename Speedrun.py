f1cfd218f6aee334f5353f9549389bec67e24a3c
Charles <44072181+Charlemagne8378@users.noreply.github.com>
2023-12-13 14:51:00 +0100

    #4
    plus de changement de format parce que le code a des erreurs sur cette partie
    Fichier JSON pour les codes des pays pour que ça soit + lisible et avoir + de données.

--- Speedrun.py
+++ Speedrun.py
@@ -1,125 +1,81 @@
-import requests
 import os
 import random
-import cv2
-import json
-import yt_dlp as youtube_dl
 import re
+import json
+import requests
+import yt_dlp as youtube_dl
+import cv2
 
-# Constantes et configurations
+# Variables + Constantes
 API_BASE_URL = "https://www.speedrun.com/api/v1"
-COUNTRY_FLAGS = {
-    "us": "🇺🇸", "br": "🇧🇷", "fr": "🇫🇷", "jp": "🇯🇵", "de": "🇩🇪",
-    "uk": "🇬🇧", "ca": "🇨🇦", "au": "🇦🇺", "it": "🇮🇹", "es": "🇪🇸",
-    "ru": "🇷🇺", "cn": "🇨🇳", "in": "🇮🇳", "mx": "🇲🇽", "kr": "🇰🇷",
-    "sa": "🇸🇦", "nl": "🇳🇱", "se": "🇸🇪", "no": "🇳🇴", "dk": "🇩🇰",
-    "fi": "🇫🇮", "pl": "🇵🇱", "za": "🇿🇦", "ch": "🇨🇭", "at": "🇦🇹",
-    "be": "🇧🇪", "ar": "🇦🇷", "cl": "🇨🇱", "co": "🇨🇴", "ve": "🇻🇪",
-    "pe": "🇵🇪", "ua": "🇺🇦", "gr": "🇬🇷", "pt": "🇵🇹", "cz": "🇨🇿",
-    "sk": "🇸🇰", "hu": "🇭🇺", "ro": "🇷🇴", "bg": "🇧🇬", "rs": "🇷🇸",
-    "hr": "🇭🇷", "si": "🇸🇮", "lv": "🇱🇻", "ee": "🇪🇪", "lt": "🇱🇹",
-    "ie": "🇮🇪", "nz": "🇳🇿", "my": "🇲🇾", "sg": "🇸🇬", "id": "🇮🇩",
-    "ph": "🇵🇭", "th": "🇹🇭", "vn": "🇻🇳", "mm": "🇲🇲", "kh": "🇰🇭",
-    "bd": "🇧🇩", "pk": "🇵🇰", "ae": "🇦🇪", "il": "🇮🇱", "eg": "🇪🇬",
-    "ng": "🇳🇬", "ke": "🇰🇪", "gh": "🇬🇭", "dz": "🇩🇿", "ma": "🇲🇦",
-    "tn": "🇹🇳", "tr": "🇹🇷", "ir": "🇮🇷", "iq": "🇮🇶", "sy": "🇸🇾",
-    "lb": "🇱🇧", "jo": "🇯🇴", "qa": "🇶🇦", "kw": "🇰🇼", "om": "🇴🇲",
-    "ye": "🇾🇪", "ge": "🇬🇪", "am": "🇦🇲", "az": "🇦🇿", "tm": "🇹🇲",
-    "uz": "🇺🇿", "kz": "🇰🇿", "kg": "🇰🇬", "tj": "🇹🇯", "mn": "🇲🇳",
-    "np": "🇳🇵", "lk": "🇱🇰", "bt": "🇧🇹", "mv": "🇲🇻", "bn": "🇧🇳",
-    "tw": "🇹🇼", "hk": "🇭🇰", "mo": "🇲🇴", "kh": "🇰🇭", "la": "🇱🇦",
-    "bd": "🇧🇩", "mm": "🇲🇲", "tl": "🇹🇱", "pg": "🇵🇬", "fj": "🇫🇯",
-    "sb": "🇸🇧", "vu": "🇻🇺", "nc": "🇳🇨", "pf": "🇵🇫", "wf": "🇼🇫",
-    "to": "🇹🇴", "tv": "🇹🇻", "ki": "🇰🇮", "nr": "🇳🇷", "ws": "🇼🇸",
-    "as": "🇦🇸", "ck": "🇨🇰", "nu": "🇳🇺", "mp": "🇲🇵", "gu": "🇬🇺",
-    "pw": "🇵🇼", "mh": "🇲🇭", "fm": "🇫🇲", "mh": "🇲🇭", "fm": "🇫🇲",
-    "ki": "🇰🇮", "nr": "🇳🇷", "mh": "🇲🇭", "pw": "🇵🇼", "as": "🇦🇸",
-    "ws": "🇼🇸", "sb": "🇸🇧", "vu": "🇻🇺", "fj": "🇫🇯", "pg": "🇵🇬",
-    "tl": "🇹🇱", "nz": "🇳🇿", "au": "🇦🇺", "ck": "🇨🇰", "tv": "🇹🇻",
-    "pf": "🇵🇫", "ki": "🇰🇮", "nc": "🇳🇨", "nu": "🇳🇺", "to": "🇹🇴",
-    "wf": "🇼🇫", "sb": "🇸🇧", "vu": "🇻🇺", "fj": "🇫🇯", "pg": "🇵🇬",
-    "tl": "🇹🇱", "nz": "🇳🇿", "au": "🇦🇺", "ck": "🇨🇰", "tv": "🇹🇻",
-    "pf": "🇵🇫", "ki": "🇰🇮", "nc": "🇳🇨", "nu": "🇳🇺", "to": "🇹🇴",
-    "wf": "🇼🇫", "sb": "🇸🇧", "vu": "🇻🇺", "fj": "🇫🇯", "pg": "🇵🇬",
-    "tl": "🇹🇱", "nz": "🇳🇿", "au": "🇦🇺", "ck": "🇨🇰", "tv": "🇹🇻",
-    "pf": "🇵🇫", "ki": "🇰🇮", "nc": "🇳🇨", "nu": "🇳🇺", "to": "🇹🇴",
-    "wf": "🇼🇫", "sb": "🇸🇧", "vu": "🇻🇺", "fj": "🇫🇯", "pg": "🇵🇬",
-    "tl": "🇹🇱", "nz": "🇳🇿", "au": "🇦🇺", "ck": "🇨🇰", "tv": "🇹🇻",
-    "pf": "🇵🇫", "ki": "🇰🇮", "nc": "🇳🇨", "nu": "🇳🇺", "to": "🇹🇴",
-    "wf": "🇼🇫", "sb": "🇸🇧", "vu": "🇻🇺", "fj": "🇫🇯", "pg": "🇵🇬",
-    "tl": "🇹🇱", "nz": "🇳🇿", "au": "🇦🇺", "ck": "🇨🇰", "tv": "🇹🇻",
-    "pf": "🇵🇫", "ki": "🇰🇮", "nc": "🇳🇨", "nu": "🇳🇺", "to": "🇹🇴",
-    "wf": "🇼🇫", "sb": "🇸🇧", "vu": "🇻🇺", "fj": "🇫🇯", "pg": "🇵🇬",
-    "tl": "🇹🇱", "nz": "🇳🇿", "au": "🇦🇺", "ck": "🇨🇰", "tv": "🇹🇻",
-    "pf": "🇵🇫", "ki": "🇰🇮", "nc": "🇳🇨", "nu": "🇳🇺", "to": "🇹🇴",
-    "wf": "🇼🇫", "sb": "🇸🇧", "vu": "🇻🇺", "fj": "🇫🇯", "pg": "🇵🇬",
-    "tl": "🇹🇱", "nz": "🇳🇿", "au": "🇦🇺", "ck": "🇨🇰", "tv": "🇹🇻",
-    "pf": "🇵🇫", "ki": "🇰🇮", "nc": "🇳🇨", "nu": "🇳🇺", "to": "🇹🇴",
-    "wf": "🇼🇫", "sb": "🇸🇧", "vu": "🇻🇺", "fj": "🇫🇯", "pg": "🇵🇬",
-    "tl": "🇹🇱", "nz": "🇳🇿", "au": "🇦🇺", "ck": "🇨🇰", "tv": "🇹🇻",
-    "pf": "🇵🇫", "ki": "🇰🇮", "nc": "🇳🇨", "nu": "🇳🇺", "to": "🇹🇴",
-    "wf": "🇼🇫", "sb": "🇸🇧", "vu": "🇻🇺", "fj": "🇫🇯", "pg": "🇵🇬",
-    "tl": "🇹🇱", "nz": "🇳🇿", "au": "🇦🇺", "ck": "🇨🇰", "tv": "🇹🇻",
-    "pf": "🇵🇫", "ki": "🇰🇮", "nc": "🇳🇨", "nu": "🇳🇺", "to": "🇹🇴",
-    "wf": "🇼🇫", "sb": "🇸🇧", "vu": "🇻🇺", "fj": "🇫🇯", "pg": "🇵🇬",
-    "tl": "🇹🇱", "nz": "🇳🇿", "au": "🇦🇺", "ck": "🇨🇰", "tv": "🇹🇻",
-}
+COUNTRY_FLAGS_FILE = "country_flags.json"
 DOWNLOAD_PATH = r"A:\Dossiers\Documents\SpeedRun_tiktok\Video_download"
 FINAL_VIDEO_PATH = r"A:\Dossiers\Documents\SpeedRun_tiktok\Video_final"
 USED_RUNS_DB_FILE = "used_runs_db.json"
 
-# Fonctions utilitaires
 def get_flag_emoji(country_code):
-    return COUNTRY_FLAGS.get(country_code.lower(), "")
+    with open(COUNTRY_FLAGS_FILE, 'r') as file:
+        country_flags = json.load(file)
+    country_data = country_flags.get(country_code.upper())
+    if country_data:
+        return country_data['emoji']
+    return "🏳️"  # Default flag for unknown country codes
+
+############################
 
 def api_get(endpoint):
-    response = requests.get(f"{API_BASE_URL}/{endpoint}")
-    return response.json() if response.ok else None
+    try:
+        response = requests.get(f"{API_BASE_URL}/{endpoint}")
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error in API request: {e}")
+        return None
 
-# Fonctions principales
 def get_games():
     response = api_get("games")
-    return {game['id']: game['names']['international'] for game in response['data']} if response else {}
+    if response:
+        return {game['id']: game['names']['international'] for game in response['data']}
+    return {}
 
 def get_player_info(player_id):
     response = api_get(f"users/{player_id}")
-    if response and 'data' in response and response['data'] is not None:
+    if response:
         player = response['data']
-        country_code = 'Inconnu'  # Valeur par défaut
-        if 'location' in player and player['location'] is not None:
-            country_code = player['location'].get('country', {}).get('code', 'Inconnu')
+        location = player.get('location') or {}
+        country = location.get('country') or {}
+        country_code = country.get('code', 'Unknown')
         return player['names']['international'], country_code
-    return "Inconnu", "Inconnu"
+    return "Unknown", "Unknown"
 
 def get_category_name(category_id):
     response = api_get(f"categories/{category_id}")
-    return response['data']['name'] if response and 'data' in response else "Inconnue"
+    if response:
+        return response['data']['name']
+    return "Unknown"
 
 def get_top_run_from_category(game_id, category_id, game_name):
     response = api_get(f"leaderboards/{game_id}/category/{category_id}?top=1")
-    if not response or 'runs' not in response['data'] or not response['data']['runs']:
-        return None
+    if response and 'runs' in response['data'] and response['data']['runs']:
+        category_name = get_category_name(category_id)
+        top_run_data = response['data']['runs'][0]['run']
+        player_id = top_run_data['players'][0]['id']
+        player_name, country_code = get_player_info(player_id)
 
-    category_name = get_category_name(category_id)
-    top_run_data = response['data']['runs'][0]['run']
-    player_id = top_run_data['players'][0]['id']
-    player_name, country_code = get_player_info(player_id)
-
-    return {
-        'game_name': game_name,
-        'category_name': category_name,
-        'player_name': player_name,
-        'country': country_code,
-        'date': top_run_data.get('date', 'Inconnue'),
-        'video_url': top_run_data.get('videos', {}).get('links', [{}])[0].get('uri', '')
-    }
+        return {
+            'game_name': game_name,
+            'category_name': category_name,
+            'player_name': player_name,
+            'country': country_code,
+            'date': top_run_data.get('date', 'Unknown'),
+            'video_url': top_run_data.get('videos', {}).get('links', [{}])[0].get('uri', '')
+        }
+    return None
 
 def safe_filename(filename):
-    """ Nettoie le nom de fichier en retirant ou remplaçant les caractères non autorisés. """
-    filename = re.sub(r'[\\/*?:"<>|]', '', filename)  # Remplacer par des caractères autorisés au besoin
-    return filename
+    return re.sub(r'[\\/*?:"<>|]', '', filename)
 
-def download_and_process_video(video_url, run_info):
+def download_video(video_url):
     ydl_opts = {
         'format': 'best',
         'outtmpl': os.path.join(DOWNLOAD_PATH, '%(title)s.%(ext)s'),
@@ -127,103 +83,63 @@ def download_and_process_video(video_url, run_info):
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(video_url, download=True)
-        video_title = safe_filename(info_dict.get('title', 'downloaded_video'))
-        video_extension = info_dict.get('ext', 'mp4')
+        downloaded_video_path = ydl.prepare_filename(info_dict)
+    return downloaded_video_path
 
-    video_filename = f"{video_title}.{video_extension}"
-    video_path = os.path.join(DOWNLOAD_PATH, video_filename)
-
-    if not os.path.exists(video_path):
-        # Essayer de trouver le fichier téléchargé en cherchant dans le dossier
-        possible_files = [f for f in os.listdir(DOWNLOAD_PATH) if f.startswith(video_title) and f.endswith(video_extension)]
-        if possible_files:
-            video_path = os.path.join(DOWNLOAD_PATH, possible_files[0])
-        else:
-            print(f"Le fichier vidéo n'existe pas : {video_path}")
-            return
-
-    final_video_filename = os.path.join(FINAL_VIDEO_PATH, video_filename)
-    add_text_to_video(video_path, final_video_filename, run_info)
-
-
-
-def add_text_to_video(video_path, final_video_path, run_info):
-    # Obtenir le drapeau du pays
+def process_video(video_path, run_info):
     country_flag = get_flag_emoji(run_info['country'])
-
-    # Construire le texte à ajouter à la vidéo
     text_content = f"{run_info['player_name']} {country_flag}\n{run_info['game_name']}\n{run_info['category_name']}\n{run_info['date']}"
 
-    # Ouvrir la vidéo originale
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
-        print(f"Erreur lors de l'ouverture de la vidéo : {video_path}")
+        print(f"Error opening video: {video_path}")
         return
 
-    # Récupérer les dimensions originales de la vidéo
     orig_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     orig_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-    # Définir les dimensions de sortie pour TikTok (9:16)
-    output_height = 1920
-    output_width = 1080
-
-    # Calculer le facteur d'échelle et les nouvelles dimensions
-    scale_factor = output_height / orig_height
-    new_width = int(orig_width * scale_factor)
-    x_margin = max((new_width - output_width) // 2, 0)
-
-    # Utiliser le codec 'mp4v' pour la compatibilité avec le format MP4
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(os.path.join(FINAL_VIDEO_PATH, safe_filename(run_info['game_name'] + '.mp4')), fourcc, cap.get(cv2.CAP_PROP_FPS), (orig_width, orig_height))
 
-    # Créer l'objet VideoWriter pour la sortie
-    out = cv2.VideoWriter(final_video_path, fourcc, cap.get(cv2.CAP_PROP_FPS), (output_width, output_height))
-
-    # Police de caractères pour le texte
     font = cv2.FONT_HERSHEY_SIMPLEX
-
-    # Traiter chaque frame
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
 
-        # Redimensionner et recadrer la frame
-        resized_frame = cv2.resize(frame, (new_width, output_height))
-        cropped_frame = resized_frame[:, x_margin:x_margin + output_width]
-
-        # Ajouter le texte à la frame
+        # Apply text to the frame
         y0, dy = 50, 40
         for i, line in enumerate(text_content.split('\n')):
-            y = y0 + i * dy
-            cv2.putText(cropped_frame, line, (50, y), font, 1, (255, 255, 255), 2)
+            cv2.putText(frame, line, (50, y0 + i * dy), font, 1, (255, 255, 255), 2)
 
-        # Écrire la frame modifiée dans le fichier de sortie
-        out.write(cropped_frame)
+        out.write(frame)
 
-    # Libérer les ressources
     cap.release()
     out.release()
 
 
-
 def main():
     games = get_games()
-    used_runs_db = json.load(open(USED_RUNS_DB_FILE, "r")) if os.path.exists(USED_RUNS_DB_FILE) else {}
+    used_runs_db = {}
+    if os.path.exists(USED_RUNS_DB_FILE):
+        with open(USED_RUNS_DB_FILE, "r") as file:
+            used_runs_db = json.load(file)
 
-    for _ in range(3):
+    try_count = 0
+    while try_count < 3:
         game_id, game_name = random.choice(list(games.items()))
         categories = api_get(f"games/{game_id}/categories")['data']
         for category in categories:
             if category['id'] not in used_runs_db.get(game_id, []):
-                # Passer game_name en tant que troisième argument
                 top_run = get_top_run_from_category(game_id, category['id'], game_name)
                 if top_run and top_run['video_url']:
-                    download_and_process_video(top_run['video_url'], top_run)
+                    video_path = download_video(top_run['video_url'])
+                    process_video(video_path, top_run)
                     used_runs_db.setdefault(game_id, []).append(category['id'])
                     break
+        try_count += 1
 
-    json.dump(used_runs_db, open(USED_RUNS_DB_FILE, "w"))
+    with open(USED_RUNS_DB_FILE, "w") as file:
+        json.dump(used_runs_db, file)
 
 if __name__ == "__main__":
     main()
